a74360ed16d2d6fe7d1a8f382a5f8513ebf9dc4c
timmaayy <tim_newth@sky.com>
2023-10-17 21:25:23 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Update the content of the span element with the current year
+document.getElementById("currentYear").innerText = currentYear;
