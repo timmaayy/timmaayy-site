76e2d6911cf87c29d01e3d4f2da06cf924a0b276
timmaayy <tim_newth@sky.com>
2023-11-09 16:37:31 +0000

    Add files via upload

--- sasg/assets/js/scripts.js
+++ sasg/assets/js/scripts.js
@@ -625,7 +625,6 @@ function generateSessionThreeOutput() {
     const endTime = document.getElementById("endTime").value;
     const altitude = document.getElementById("altitude").value;
     const airspace = document.getElementById("airspace").value;
-    const weather = document.getElementById("weather").value;
     const flightCheck = document.getElementById("flightCheck").value;
     const flightCheckGrade = document.getElementById("flightCheckGrade").value;
     const studentRoleplay = document.getElementById("studentRoleplay").value;
@@ -784,6 +783,160 @@ Go through the checklist below, and mark each section with a [ x ] once complete
 }
 
 function clearSessionThreeForm() {
+    // Refresh the page
+    location.reload();
+}
+
+/* Flight Session4 */
+function generateSessionFourOutput() {
+    // Retrieve input values
+
+    const date = document.getElementById("date").value;
+    const formattedDateTime = moment(date).format('DD/MMM/YYYY').toUpperCase();
+    const startTime = document.getElementById("startTime").value;
+    const endTime = document.getElementById("endTime").value;
+    const flightCheck = document.getElementById("flightCheck").value;
+    const flightCheckGrade = document.getElementById("flightCheckGrade").value;
+    const studentRoleplay = document.getElementById("studentRoleplay").value;
+    const call1 = document.getElementById("call1").value;
+    const call2 = document.getElementById("call2").value;
+    const call3 = document.getElementById("call3").value;
+    const call4 = document.getElementById("call4").value;
+    const call5 = document.getElementById("call5").value;
+    const call6 = document.getElementById("call6").value;
+    const call7 = document.getElementById("call7").value;
+    const call8 = document.getElementById("call8").value;
+    const call9 = document.getElementById("call9").value;
+    const sessionNotes = document.getElementById("sessionNotes").value;
+    const behaviour = document.getElementById("behaviour").value;
+    const behaviourDetails = document.getElementById("behaviourDetails").value;
+    const takeOff = document.getElementById("takeOff").value;
+    const takeOffDetails = document.getElementById("takeOffDetails").value;
+    const directionalFlight = document.getElementById("directionalFlight").value;
+    const directionalFlightDetails = document.getElementById("directionalFlightDetails").value;
+    const groundLanding = document.getElementById("groundLanding").value;
+    const groundLandingDetails = document.getElementById("groundLandingDetails").value;
+    const figureEight = document.getElementById("figureEight").value;
+    const figureEightDetails = document.getElementById("figureEightDetails").value;
+    const taxiing = document.getElementById("taxiing").value;
+    const taxiingDetails = document.getElementById("taxiingDetails").value;
+    const approachLanding = document.getElementById("approachLanding").value;
+    const approachLandingDetails = document.getElementById("approachLandingDetails").value;
+    const airTrafficControl = document.getElementById("airTrafficControl").value;
+    const airTrafficControlDetails = document.getElementById("airTrafficControlDetails").value;
+    const proceed = document.getElementById("proceed").value;
+
+    // Generate output message
+    const message = "[img]https://i.imgur.com/RHrjgV2.png[/img]\n\
+[divbox=white]\n\
+Date: " + formattedDateTime + "\n\
+Session Hours ((Please use UTC: https://time.is/UTC )): " + startTime + " > " + endTime + "\n\
+[/divbox]\n\
+\n\
+[divbox=#091f62][b][color=white]SESSION DETAILS[/color][/b][/divbox]\n\
+\n\
+[divbox=white]\n\
+Go through the checklist below, and mark each section with a [ x ] once completed. (YOU MUST GO THROUGH ALL OF THEM)\n\
+\n\
+[b]Grading System[/b]\n\
+[size=125]■[/size] - [dropshadow=black][color=#FF0000]Below satisfactory performance.[/color][/dropshadow]\n\
+[size=125]■■[/size] - [dropshadow=black][color=#FF8000]Needs some minor improvements.[/color][/dropshadow]\n\
+[size=125]■■■[/size] - [dropshadow=black][color=#00FF00]Satisfactory. [/color][/dropshadow]\n\
+[size=125]■■■■[/size] - [dropshadow=black][color=#00BF80]Outstanding, no room for improvements.[/color][/dropshadow]\n\
+[size=125]■■■■■[/size] - [dropshadow=black][color=#008040]Extraordinary[/color][/dropshadow]\n\
+[/divbox]\n\
+\n\
+[divbox=#091f62][b][color=white]Pre-Flight Check[/color][/b][/divbox]\n\
+[divbox=white]\n\
+[b]The instructor showed the student how to perform a pre-flight check during FTS-2. They should have been able to perform their pre-flight check during FTS-3. Do not tell them they need to do a pre-flight check, see if they will do one without saying anything.[/b]\n\
+\n\
+" + flightCheck + " The student remembered performing a pre-flight check on the aircraft internally and externally.\n\
+[list=none]GRADE: " + flightCheckGrade + "[/list]\n\
+[ooc][spoiler=Student's RP]\n\
+" + studentRoleplay + "\n\
+[/spoiler][/ooc]\n\
+\n\
+[/divbox]\n\
+[divbox=#091f62][b][color=white]ATC Communication[/color][/b][/divbox]\n\
+[divbox=white]\n\
+[b]SAAA Hangar --> LSIA Helipad One (taxi) --> Cayo Perico Helipad --> Grapeseed Runway Helipad --> Paleto North Point Rock --> LSIA Figure 8's --> LSIA Helipad One (taxi)[/b]\n\
+[hr][/hr]\n\
+[b]Please write their exact phrasing of communication with ATC word for word. There should be a total of [b]9[/b] calls to ATC.[/b]\n\
+\n\
+[list]\n\
+[*] " + call1 + "\n\
+[*] " + call2 + "\n\
+[*] " + call3 + "\n\
+[*] " + call4 + "\n\
+[*] " + call5 + "\n\
+[*] " + call6 + "\n\
+[*] " + call7 + "\n\
+[*] " + call8 + "\n\
+[*] " + call9 + "\n\
+[/list]\n\
+\n\
+[/divbox]\n\
+[divbox=#091f62][b][color=white]Session Notes[/color][/b][/divbox]\n\
+[divbox=white]\n\
+[b]As the flight instructor, you are required to give a detailed report regarding the final flight session below. Do they enter any restricted or prohibited airspaces during the flight? How often do they try to ask you questions regarding what they should be doing or if they do something correctly? If they do not seem confident in their ability to fly without supervision, they are not ready to obtain a Pilot's License. Instead of grading each travel, you can grade their overall abilities combined in the final section. [/b] [color=red]MINIMUM 75 WORDS[/color]\n\
+[hr][/hr]\n\
+\n\
+" + sessionNotes + " \n\
+\n\
+[color=transparent]SPACER[/color]\n\
+[/divbox]\n\
+[divbox=#091f62][b][color=white]Overall Grade[/color][/b][/divbox]\n\
+[divbox=white]\n\
+\n\
+[list]\n\
+[*] The student's general behavior and attention span: " + behaviour + "\n\
+[list]\n\
+[*] " + behaviourDetails + "\n\
+[/list]\n\
+[list]\n\
+[*] The student's ability to perform a take-off: " + takeOff + "\n\
+[list]\n\
+[*] " + takeOffDetails + "\n\
+[/list]\n\
+[*] The student's ability to perform directional flight: " + directionalFlight + "\n\
+[list]\n\
+[*] " + directionalFlightDetails + "\n\
+[/list]\n\
+[*] The student's ability to perform an approach and landing: " + approachLanding + "\n\
+[list]\n\
+[*] " + approachLandingDetails + "\n\
+[/list]\n\
+[*] The student's ability to land at a random location (Paleto North Point Rock): " + groundLanding + "\n\
+[list]\n\
+[*] " + groundLandingDetails + "\n\
+[/list]\n\
+[*] The student's ability to maneuver (LSIA Figure 8's): " + figureEight + "\n\
+[list]\n\
+[*] " + figureEightDetails + "\n\
+[/list]\n\
+[*] The student's ability to taxi from place to place: " + taxiing + "\n\
+[list]\n\
+[*] " + taxiingDetails + "\n\
+[/list]\n\
+[*] The student's ability to communicate with Air Traffic Control: " + airTrafficControl + "\n\
+[list]\n\
+[*] " + airTrafficControlDetails + "\n\
+[/list]\n\
+[*] Do you believe this student should pass their final flight? Do you believe they can handle themselves without supervision? [color=red]MINIMUM 40 WORDS[/color]\n\
+[list]\n\
+[*] " + proceed + "\n\
+[/list]\n\
+[/divbox]";
+
+    // Display certificate output message
+    document.getElementById("reportOutput").value = message;
+
+    const copyText = document.getElementById("reportOutput");
+    copyText.select();
+    document.execCommand("copy");
+}
+
+function clearSessionFourForm() {
     // Refresh the page
     location.reload();
 }
